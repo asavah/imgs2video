8428891300bc2be12e84e536b63aa4b22fd268db
root <asavah@avh.od.ua>
2018-08-21 00:37:28 +0300

    fix fps, fix filter passing via argv, restore some flags

--- assemble_and_compress.sh
+++ assemble_and_compress.sh
@@ -33,6 +33,10 @@ fi
 IMGDIR=`readlink -f $1`
 OUTFILE=`readlink -f $2`
 
+if [ -n "$3" ]; then
+FILTER=$3
+fi
+
 if [ -z "$FILTER" ] || [ "x$FILTER" = "xnull" ]; then
 FILTER=""
 else
@@ -49,12 +53,15 @@ trap cleanup INT TERM QUIT
 
 echo Making $OUTFILE of $IMGDIR
 $FFMPEG \
-        -r $FRAMERATE \
+        -err_detect explode \
+        -f image2 \
         -ts_from_file 1 \
         -pattern_type glob \
         -i $IMGDIR'/*.jpg' \
-        -vf "${FILTER}settb=1/1000,setpts='(PTS-STARTPTS)/$SPEEDUP',fps=$FRAMERATE" \
+        -vf "${FILTER}settb=1/1000,setpts=(PTS-STARTPTS)/$SPEEDUP" \
         $VIDEO_ENCODING_OPTS \
+        -r $FRAMERATE \
+        -an \
         -y \
         $OUTFILE
 RET=$?
